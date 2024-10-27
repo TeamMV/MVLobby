66291db8840248b3d92e37fca59dce78f3ed1f12
v22 <v22julian@gmail.com>
2024-10-27 23:11:01 +0100

    fix player invis

--- src/main/java/dev/mv/lobby/JoinListener.java
+++ src/main/java/dev/mv/lobby/JoinListener.java
@@ -4,8 +4,10 @@ import dev.mv.lobby.components.NPC;
 import dev.mv.lobby.conf.LobbyConfig;
 import dev.mv.lobby.game.LobbyGame;
 import dev.mv.lobby.scoreboard.LobbyScoreboard;
+import dev.mv.ptk.Ptk;
 import dev.mv.ptk.Utils;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -16,13 +18,19 @@ public class JoinListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         new LobbyScoreboard(e.getPlayer());
-        e.getPlayer().teleport(LobbyConfig.getInstance().getLobbySpawn());
-        e.setJoinMessage(Utils.chat("&2&lWelcome to mvteam.dev!"));
+        Bukkit.getScheduler().runTaskLater(Lobby.INSTANCE, () -> {
+            e.getPlayer().teleport(LobbyConfig.getInstance().getLobbySpawn());
+        }, 1);
+        e.setJoinMessage(Utils.chat("&2&l%s joined mvteam.dev!", e.getPlayer().getName()));
+
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            p.showPlayer(Lobby.INSTANCE, e.getPlayer());
+        }
     }
 
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent e) {
-        LobbyScoreboard.SCOREBOARDS.remove(e.getPlayer());
+        LobbyScoreboard.SCOREBOARDS.remove(e.getPlayer()).setCurrentGame(null);
     }
 
     public void onServerReload(ServerLoadEvent e) {
